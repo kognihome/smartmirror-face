92b1b6b56ff34324641dcaea98227408aaec1d2f
Alexander Neumann <aleneum@gmail.com>
2017-07-14 17:40:26 +0200

    skip openface and rsb-python requirements since they might actually install wrong versions

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ setup(
     author='Alexander Neumann',
     author_email='alneuman@techfak.uni-bielefeld.de',
     packages=["smartmirror_face"],
-    install_requires=['openface', 'rsb-python'],
     entry_points={
       'console_scripts': [
           'smartface = smartmirror_face.main:start'
